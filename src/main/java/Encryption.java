77dfa138f20bdb7c01358ec7f32a8d39ff256d38
joaopupo <joaopupo@sapo.pt>
2023-05-08 11:20:07 +0100

    chore(Block encryption mode) : replacement the mode of symmetric encryption from ECB to CBC

--- src/main/java/Encryption.java
+++ src/main/java/Encryption.java
@@ -1,4 +1,5 @@
 import javax.crypto.*;
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.ByteBuffer;
 import java.security.*;
@@ -24,35 +25,80 @@ public class Encryption {
         return cipher.doFinal ( message );
     }
 
-    /**
-     * @param message   the message to be encrypted
-     * @param secretKey the secret key used to encrypt the message
-     *
-     * @return the encrypted message as an array of bytes
-     *
-     * @throws Exception when the decryption fails
-     */
-    public static byte[] encryptMessage(byte[] message , byte[] secretKey, String algorithm ) throws Exception {
-        byte[] secretKeyPadded = ByteBuffer.allocate ( keySize(algorithm)/8 ).put ( secretKey ).array ( );
-        SecretKeySpec secreteKeySpec = new SecretKeySpec ( secretKeyPadded , algorithm );
-        Cipher cipher = Cipher.getInstance ( algorithm + "/ECB/PKCS5Padding" );
-        cipher.init ( Cipher.ENCRYPT_MODE , secreteKeySpec );
-        return cipher.doFinal ( message );
-    }
+//    /**
+//     * @param message   the message to be encrypted
+//     * @param secretKey the secret key used to encrypt the message
+//     *
+//     * @return the encrypted message as an array of bytes
+//     *
+//     * @throws Exception when the decryption fails
+//     */
+//    public static byte[] encryptMessage(byte[] message , byte[] secretKey, String algorithm ) throws Exception {
+//        byte[] secretKeyPadded = ByteBuffer.allocate ( keySize(algorithm)/8 ).put ( secretKey ).array ( );
+//        SecretKeySpec secreteKeySpec = new SecretKeySpec ( secretKeyPadded , algorithm );
+//        Cipher cipher = Cipher.getInstance ( algorithm + "/ECB/PKCS5Padding" );
+//        cipher.init ( Cipher.ENCRYPT_MODE , secreteKeySpec );
+//        return cipher.doFinal ( message );
+//    }
+
 
     /**
-     * @param message   the message to be decrypted with AES256 Algorithm
-     * @param secretKey the secret key used to decrypt the message
-     * @return the decrypted message as an array of bytes
-     * @throws Exception when the encryption fails
+     * Encrypts a text using AES in CBC (Cipher Block Chaining) mode.
+     * The initializatiom vector is created with a random value with the same length of the block size,
+     * @param message   the message to be encrypted
+     * @param secretKey the secret key used to encrypt the message
+     * @param algorithm AES, DES or 3DES
+     * @return          the encrypted message as an array of bytes
+     * @throws Exception
      */
-    public static byte[] decryptMessage(byte[] message , byte[] secretKey, String algorithm  ) throws Exception {
-        byte[] secretKeyPadded = ByteBuffer.allocate ( keySize(algorithm)/8 ).put ( secretKey ).array ( );
-        SecretKeySpec secreteKeySpec = new SecretKeySpec ( secretKeyPadded , algorithm );
-        Cipher cipher = Cipher.getInstance ( algorithm + "/ECB/PKCS5Padding" );
-        cipher.init ( Cipher.DECRYPT_MODE , secreteKeySpec );
-        return cipher.doFinal ( message );
+    public static byte[] encryptMessage(byte[] message, byte[] secretKey, String algorithm) throws Exception {
+        byte[] secretKeyPadded = Arrays.copyOf(secretKey, keySize(algorithm) / 8);
+        SecretKeySpec secretKeySpec = new SecretKeySpec(secretKeyPadded, algorithm);
+        Cipher cipher = Cipher.getInstance(algorithm + "/CBC/PKCS5Padding");
+        byte[] iv = new byte[cipher.getBlockSize()];    // iv size = BlockSize
+        SecureRandom secureRandom = new SecureRandom();
+        secureRandom.nextBytes(iv);
+        IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
+        cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, ivParameterSpec);
+        byte[] encryptedMessage = cipher.doFinal(message);
+        byte[] encryptedMessageWithIV = new byte[encryptedMessage.length + iv.length];
+        System.arraycopy(iv, 0, encryptedMessageWithIV, 0, iv.length);
+        System.arraycopy(encryptedMessage, 0, encryptedMessageWithIV, iv.length, encryptedMessage.length);
+        return encryptedMessageWithIV;
     }
+
+//    /**
+//     * @param message   the message to be decrypted with AES256 / DES / 3DES Algorithm
+//     * @param secretKey the secret key used to decrypt the message
+//     * @return the decrypted message as an array of bytes
+//     * @throws Exception when the encryption fails
+//     */
+//    public static byte[] decryptMessage(byte[] message , byte[] secretKey, String algorithm  ) throws Exception {
+//        byte[] secretKeyPadded = ByteBuffer.allocate ( keySize(algorithm)/8 ).put ( secretKey ).array ( );
+//        SecretKeySpec secreteKeySpec = new SecretKeySpec ( secretKeyPadded , algorithm );
+//        Cipher cipher = Cipher.getInstance ( algorithm + "/ECB/PKCS5Padding" );
+//        cipher.init ( Cipher.DECRYPT_MODE , secreteKeySpec );
+//        return cipher.doFinal ( message );
+//    }
+
+    /**
+     * Dencrypts a text using AES in CBC (Cipher Block Chaining) mode.
+     * @param Message   the message to be decrypted
+     * @param secretKey secretKey the secret key used to decrypt the message
+     * @param algorithm AES, DES or 3DES
+     * @return          the decrypted message as an array of bytes
+     * @throws Exception
+     */
+    public static byte[] decryptMessage(byte[] Message, byte[] secretKey, String algorithm) throws Exception {
+        byte[] secretKeyPadded = ByteBuffer.allocate(keySize(algorithm) / 8).put(secretKey).array();
+        SecretKeySpec secretKeySpec = new SecretKeySpec(secretKeyPadded, algorithm);
+        Cipher cipher = Cipher.getInstance(algorithm + "/CBC/PKCS5Padding");
+        byte[] iv = Arrays.copyOfRange(Message, 0, cipher.getBlockSize());
+        IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
+        cipher.init(Cipher.DECRYPT_MODE, secretKeySpec, ivParameterSpec);
+        return cipher.doFinal(Arrays.copyOfRange(Message, cipher.getBlockSize(), Message.length));
+    }
+
 
     // KeySize assigned for each algorithm
     private static int keySize(String algorithm) {
